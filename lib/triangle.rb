ccf0a55d0e5e05ab28599a1a8c3ee361e59c79f3
Isaiah <ijandicos90@gmail.com>
2022-07-21 19:42:13 -0400

    commit tree bafc936011e290c3553468fe9b6cd21cac7502b5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,37 +1,26 @@
 require "pry"
 
 class Triangle
-  attr_reader :side_1, :side_2, :side_3
+
+  attr_reader :a, :b, :c
   
-  def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    validate_triangle
-    if side_1 == side_2 && side_2 == side_3
-      :equilateral
-    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
-      :isosceles
-    else
-      :scalene
+    [a, b, c].permutation do |sides|
+      raise TriangleError unless sides[0] + sides[1] > sides[2]
+    end
+    case [a,b,c].uniq.size
+      when 3; :scalene
+      when 2; :isosceles
+      when 1; :equilateral
     end
   end
-
-  def sides_greater_than_zero?
-    [side_1, side_2, side_3].all?(&:positive?)
-  end
-
-  def two_sides_are_greater?
-    side_1 + side_2 > side_3 && side_1 + side_3 > side_2 && side_2 + side_3 > side_1
-  end
-
-  def validate_triangle
-    raise TriangleError unless sides_greater_than_zero? && two_sides_are_greater?
-  end
-
+  
   class TriangleError < StandardError
   end
 
